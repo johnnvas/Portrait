7a50f30ad5347a4c7bed28270055f1a269a6c6df
Johnn <j.christian.vasquez@gmail.com>
2022-04-17 14:36:34 -0500

    successful console.log of planets

--- portrait/src/components/InputField.tsx
+++ portrait/src/components/InputField.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import axios, {AxiosResponse} from 'axios';
 import './styles.css'
 
 interface Props{
@@ -9,6 +10,24 @@ interface Props{
 
 const InputField = ({ planet, setPlanet, handleAdd }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const getPlanets = async (planet: string) => {
+    const res = await axios(`https://swapi.dev/api/planets/?search=${planet}`);
+    console.log('HEEEERREEEEEEEEE', res.data);
+    // return res;
+    // setData(res.data);
+  };
+
+  // const getPlanets = async (): Promise<Array<Planet> | string> => {
+  //   const api = `https://swapi.dev/api/planets?search=r2`
+  //   const res = await axios(api);
+  //     // const response = await fetch(api)
+  //   console.log('HEEEERREEEEEEEEE', res.data);
+  //     // const { data } = await res.json()
+  //     // return data
+
+  // }
+
   return (
     <form
       className='input'
@@ -24,7 +43,10 @@ const InputField = ({ planet, setPlanet, handleAdd }: Props) => {
       placeholder='Search A Planet'
       className='input__box'
       onChange={
-        (e)=> setPlanet(e.target.value)
+        (e)=> {
+          setPlanet(e.target.value)
+          getPlanets(e.target.value)
+        }
       }
       />
       <button className='input_submit' type='submit'>
--- portrait/src/index.tsx
+++ portrait/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
@@ -12,4 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
